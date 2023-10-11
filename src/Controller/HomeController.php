a58a37d5baa624eb93b609d8f4f349cde12f1310
alexis <alexissimoes94@gmail.com>
2023-10-11 16:32:01 +0200

    admin go

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAll();
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
+            'articles' => $articles,
+        ]);
+    } 
+    #[Route('/show/{id}', name: 'app_show')]
+    public function show(ArticleRepository $articleRepository, $id): Response
+    {
+        $article = $articleRepository->find($id);
+
+        return $this->render('home/show.html.twig', [
+            'controller_name' => 'HomeController',
+            'article' => $article,
         ]);
     } 
 }
