e63c55656d29183900691ba991ef4e1c6728dd02
alexis <alexissimoes94@gmail.com>
2023-10-17 16:51:22 +0200

    form validator

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,22 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(ArticleRepository $articleRepository): Response
+    public function index(ArticleRepository $articleRepository, CategoryRepository $categoryRepository): Response
     {
         $articles = $articleRepository->findAll();
+        $categories = $categoryRepository->findAll();
+
+        //dd($articles);
+
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+            'hello_world' => 'Hello world!',
             'articles' => $articles,
+            'categories' => $categories
         ]);
-    } 
+    }
     #[Route('/show/{id}', name: 'app_show')]
     public function show(ArticleRepository $articleRepository, $id): Response
     {
@@ -28,4 +35,26 @@ class HomeController extends AbstractController
             'article' => $article,
         ]);
     } 
+    #[Route('/showArticleCategory/{id}', name: 'show_articles_category')]
+    public function showArticlesCategorie(Category $categorie, CategoryRepository $categoryRepository): Response
+    {
+
+        $categories = $categoryRepository->findAll();
+
+        if($categorie) {
+
+            $articles = $categorie->getArticles()->getValues();
+
+        }else {
+
+            return $this->redirectToRoute('app_home');
+        }
+        // $categorie = $categoryRepository->find($id);
+        //dd($articles);
+
+        return $this->render('home/home.html.twig', [
+            'articles' => $articles,
+            'categories' => $categories
+        ]);
+    }
 }
